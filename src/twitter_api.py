f389d21b44c5cfd9e2c1491f183700959197debe
lilitmovsesian <lilitmovsesian@proton.me>
2024-08-14 15:19:44 +0200

    feat: adding only non-retweets to the db

--- src/twitter_api.py
+++ src/twitter_api.py
@@ -7,4 +7,6 @@ class TwitterAPI:
     
     def fetch_tweets(self, query):
         tweets = self.client.search_recent_tweets(query=query, tweet_fields=['id', 'text', 'created_at'], max_results=10)
-        return [Tweet(id = tweet.id,text=tweet.text, created_at=tweet.created_at) for tweet in tweets.data]
+        if tweets.data is None:
+            return []
+        return [Tweet(id = tweet.id,text=tweet.text, created_at=tweet.created_at) for tweet in tweets.data if not tweet.text.startswith('RT @')]
