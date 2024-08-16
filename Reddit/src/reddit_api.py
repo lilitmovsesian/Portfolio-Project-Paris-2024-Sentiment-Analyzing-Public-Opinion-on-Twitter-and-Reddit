03815d625ac7fcf044c7066a43ff4ceec574b786
lilitmovsesian <lilitmovsesian@proton.me>
2024-08-16 14:41:42 +0200

    feat: collected more data, improved subreddit filtering

--- Reddit/src/reddit_api.py
+++ Reddit/src/reddit_api.py
@@ -7,22 +7,25 @@ class RedditAPI:
     
     def fetch_posts(self, query):
             
-        subreddit_list = ['all']
         posts = []
-        for subreddit_name in subreddit_list:
-            subreddit = self.reddit.subreddit(subreddit_name)
-            search_results = subreddit.search(query, sort='new', time_filter='year', limit=1000)
-            
-            for submission in search_results:
-                id = submission.id
-                title = submission.title
-                text = None
-                if submission.selftext:
-                    text = submission.selftext
-                url = submission.url
-                score = submission.score
-                subreddit_name = submission.subreddit.display_name
+        subreddit = self.reddit.subreddit('olympics')
+        #search_results = subreddit.search(query, sort='comments', time_filter='year', limit=250)
+        #search_results = subreddit.top(time_filter='year', limit=250)
+        #search_results = subreddit.hot(limit=250)
+        search_results = subreddit.new(limit=250)
+
+        for submission in search_results:
+            id = submission.id
+            title = submission.title
+            text = None
+            if submission.selftext:
+                text = submission.selftext
+            url = submission.url
+            score = submission.score
+            subreddit_name = submission.subreddit.display_name
+            author = None
+            if submission.author:
                 author = submission.author.name
-                created_at = submission.created_utc
-                posts.append(Post(id = id,text=text, created_at=created_at, author = author, subreddit_name = subreddit_name, score = score, url = url, title = title))
+            created_at = submission.created_utc
+            posts.append(Post(id = id,text=text, created_at=created_at, author = author, subreddit_name = subreddit_name, score = score, url = url, title = title))
         return posts
